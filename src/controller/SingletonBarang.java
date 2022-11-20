fe95dc63416dba9cc2e458b59ccd3994cd65f618
MIchael Setiawan <michaelsetiawan686@gmail.com>
2022-11-21 01:22:38 +0700

    add menu product details

--- src/controller/SingletonBarang.java
+++ src/controller/SingletonBarang.java
@@ -32,6 +32,10 @@ public class SingletonBarang {
     public SingletonBarang() {
         barang = new Barang(0, null, 0, 0, null, 0, 0);
     }
+    
+    public void reset(){
+        barang = new Barang();
+    }
 
     public Vector<Barang> getAllBarang() {
         conn.connect();
@@ -86,10 +90,49 @@ public class SingletonBarang {
         return barangVector;
     }
 
-//    public Barang getProductDetails(int productId) {
-//        
-//    }
-//    public double getAverage(int prouductId) {
-//        
-//    }
+    public Barang getProductDetails(int productId) {
+        conn.connect();
+
+        try {
+            java.sql.Statement stat = conn.con.createStatement();
+            ResultSet result = stat.executeQuery("SELECT * FROM barang WHERE id_barang ='" + productId + "'");
+            if (result.next()) {
+                int id_barang = result.getInt("id_barang");
+                String nama_barang = result.getString("nama_barang");
+                int stok_barang = result.getInt("stok_barang");
+                double harga_barang = result.getDouble("harga_barang");
+                String deskripsi_barang = result.getString("deskripsi_barang");
+                int jumlah_pengunjung = result.getInt("jumlah_pengunjung");
+                int status = result.getInt("status");
+
+                Barang barang = new Barang(id_barang, nama_barang, stok_barang, harga_barang, deskripsi_barang, jumlah_pengunjung,
+                        status);
+                return barang;
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error occured when connecting to database.");
+        }
+        return null;
+
+    }
+
+    public double getAverage(int productId) {
+        conn.connect();
+        try {
+            java.sql.Statement stat = conn.con.createStatement();
+            ResultSet result = stat.executeQuery("SELECT * FROM transaksi WHERE id_barang ='" + productId + "'");
+            double ratingTotal = 0;
+            int counter = 0;
+            while(result.next()) {
+                Double rating = result.getDouble("rating");
+                ratingTotal += rating;
+                counter++;
+            }
+            return ratingTotal/counter;
+
+        } catch (SQLException e) {
+                // TODO: handle exception
+        }
+        return 0;
+    }
 }
